3a5156b5d8404356525e2d625421add8b6514e7a
zencyy <zennchuayz@gmail.com>
2025-06-12 15:38:12 +0800

    Added the "Press E to collect" function for my collectible, added UI text that will appear everytime the player enters the collider

--- Assets/Script/CollectibleItem.cs
+++ Assets/Script/CollectibleItem.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
-
-public class NewMonoBehaviourScript : MonoBehaviour
+using TMPro;
+public class CollectibleItem : MonoBehaviour
 {
+    public int itemScore = 1;
+    private bool isPlayerNear = false;
+
+    private TextMeshProUGUI promptText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        promptText = GetComponentInChildren<TextMeshProUGUI>(true);
+        if (promptText != null)
+            promptText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Collected Item! +" + itemScore + "points");
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+            if (promptText != null)
+                promptText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            if (promptText != null)
+                promptText.gameObject.SetActive(false);
+        }
     }
 }
